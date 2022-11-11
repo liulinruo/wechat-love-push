8d37704be563f899bcfc5babeaa43a687a1d7822
liulinruo <52908913+liulinruo@users.noreply.github.com>
2022-11-11 16:55:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -10,12 +10,6 @@ const USER_CONFIG = {
   // 请将以下代码按照图示粘贴到对应位置
   USE_PASSAGE: 'push-plus',
 
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
-
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
-
   PROVINCE: '广东',
   CITY: '广州绿',
 
@@ -25,8 +19,6 @@ const USER_CONFIG = {
       name: '最爱的咕咕',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'f1e7c2a2b6044a1a96dc863d8c3ee7c0',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-10',
       festivals: [
