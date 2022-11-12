de5963dfc768880d82b75f8b89faad34936f87bd
liulinruo <52908913+liulinruo@users.noreply.github.com>
2022-11-12 20:57:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -42,7 +42,34 @@ const USER_CONFIG = {
         { keyword: 'love_day', date: '2020-11-24' },
       ],
     },
-    
+    {
+      // 想要发送的人的名字
+      name: '最爱的咕咕',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'b9ef85cb65694facb18bcbdd208280fa',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '0001',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '07-10',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '生日', name: '古雯惠', year: '2001', date: '05-20',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '刘林若', year: '2000', date: '07-24',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2020', date: '11-24',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2020-11-24' },
+      ],
+    },
   ],
   
   
